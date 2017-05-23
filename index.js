0cc2e70390c8e5e60113170acaaecb4d11fffb9a
Joel Brubaker <joel@tipsyelves.com>
2017-05-23 14:34:48 -0700

    Created factory and tests passing

---
+++ index.js
@@ -0,0 +1,89 @@
+const faker = require('faker')
+const registeredFakes = {}
+
+class FakerInstance {
+  /**
+   * Constructor for the class.
+   * @param  {class} model  The type of model to initialize.
+   * @param  {object} attributes  The fake attributes.
+   * @return {FakerInstance}
+   */
+  constructor (model, attributes) {
+    this.model = model
+    this.attributes = attributes
+    this.quantity = 1
+  }
+
+  /**
+   * Make a new model.
+   * @param  {object} attributes  Attributes to override the faker data.
+   * @return {object|array}
+   */
+  create (attributes = {}) {
+    let modelOrArray = this.make(attributes)
+    return Array.isArray(modelOrArray) ? modelOrArray.map(model => model.create()) : modelOrArray.create()
+  }
+
+  /**
+   * Make a new model.
+   * @param  {object} attributes  Attributes to override the faker data.
+   * @return {object|array}
+   */
+  make (attributes) {
+    if (this.quantity === 1) {
+      return this.createModel(attributes)
+    }
+
+    const models = []
+    for (let i = 0; i < this.quantity; i++) {
+      models.push(this.createModel(attributes))
+    }
+    return models
+  }
+
+  /**
+   * Create a new instance of the model.
+   * @param  {object} attributes  The attributes to fill in the model.
+   * @return {object}
+   */
+  createModel (attributes) {
+    return new this.model(Object.assign(this.attributes, attributes))
+  }
+}
+
+/**
+ * A function for generating fake instances of models.
+ * @param  {string} label  The type of fake to make.
+ * @param  {integer} quantity  The number of fakes to make.
+ * @return {FakerInstance}  A faker instance.
+ */
+function factory (label, quantity = 1) {
+  let faker = registeredFakes[label]
+  faker.quantity = quantity
+  
+  return faker
+}
+
+/**
+ * Register a new factory.
+ * @param  {string}  label  The label for the factory.
+ * @param  {class}  model  The type of object to create.
+ * @param  {Function}  callback  A callback that returns an array of faker data.
+ */
+factory.register = (label, model, callback) => {
+  registeredFakes[label] = new FakerInstance(model, callback(faker))
+}
+
+/**
+ * Get a registered faker instance with the factory.
+ * @param  {string} label  The label of the faker instance.
+ * @return {FakerInstance}  The faker instance.
+ */
+factory.getRegisteredFaker = (label) => {
+  return registeredFakes[label]
+}
+
+factory.FakerInstance = FakerInstance
+factory.faker = faker
+
+module.exports = factory
---
+++ test/factory.spec.js
@@ -0,0 +1,71 @@
+const assert = require('assert')
+const factory = require('../index.js')
+
+class TestObject {
+  constructor (attributes = {}) {
+    Object.assign(this, attributes)
+    this.created = false
+  }
+
+  create () {
+    this.created = true
+    return this
+  }
+}
+
+describe('factory', function() {
+  it('should register a new factory', function() {
+    factory.register('foo', TestObject, faker => {
+      return {
+        foo: 'bar'
+      }
+    })
+    let instance = factory.getRegisteredFaker('foo')
+    assert(instance.quantity === 1)
+    assert(instance.attributes.foo === 'bar')
+    assert(instance.model === TestObject)
+  })
+  it('should make a single new instance from the factory', function() {
+    factory.register('foo', TestObject, faker => {
+      return {
+        foo: 'bar'
+      }
+    })
+    let object = factory('foo').make()
+    assert(object.constructor.name === 'TestObject')
+    assert(object.foo === 'bar')
+  })
+  it('should make many new instances from the factory', function() {
+    factory.register('foo', TestObject, faker => {
+      return {
+        foo: faker.name
+      }
+    })
+    let collection = factory('foo', 4).make()
+    assert(collection.length === 4)
+    assert(collection[0].constructor.name === 'TestObject')
+  })
+  it('should create a single new instance from the factory', function() {
+    factory.register('foo', TestObject, faker => {
+      return {
+        foo: 'bar'
+      }
+    })
+    let object = factory('foo').create()
+    assert(object.constructor.name === 'TestObject')
+    assert(object.foo === 'bar')
+    assert(object.created === true)
+  })
+  it('should create many new instances from the factory and override attributes', function() {
+    factory.register('foo', TestObject, faker => {
+      return {
+        foo: 'bar'
+      }
+    })
+    let collection = factory('foo', 4).create({ foo: 'baz' })
+    assert(collection.length === 4)
+    assert(collection[0].constructor.name === 'TestObject')
+    assert(collection[0].foo === 'baz')
+    assert(collection[0].created === true)
+  })
+})
